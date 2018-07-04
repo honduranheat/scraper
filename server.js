e35b255edad0ba5a116ef24013e1fa61cb099185
Justin <jwhittington92@yahoo.com>
2018-07-04 15:38:37 -0400

    hello heroku

--- server.js
+++ server.js
@@ -27,10 +27,18 @@ app.engine('handlebars', handlebars({
 
 app.set('view engine', 'handlebars');
 
-mongoose.connect("mongodb://localhost/week18Populater");
+
 
 require('./routes/index')(app);
 
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 app.listen(PORT, function () {
